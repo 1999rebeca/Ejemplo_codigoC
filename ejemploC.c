abc317a2c2605579321fe76774ee992dc4f36610
rebeca <rebecabargiela@gmail.com>
2022-10-10 16:58:44 +0200

    primer commit

---
+++ ejemploC.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+int funcionSuma(int a, int b);
+
+void main() {
+	
+	int n1, n2, suma;
+	
+	n1 = 9;
+	n2 = 3;
+	
+	printf("\n n1= %d, n1");
+	printf("\n n2= %d, n2");
+	
+	
+	suma = funcionSuma(n1,n2);
+	printf("\n La suma es: %d", suma);
+}
+
+int funcionSuma (int a, int b) {
+	return a + b;
+
+
+}
